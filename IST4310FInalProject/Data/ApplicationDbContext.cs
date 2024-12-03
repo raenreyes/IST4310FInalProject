3fd589eebf225c6f63b2e8861bce6c588bcb9607
raenreyes <raen.reyes11@outlook.com>
2024-12-03 13:13:51 -0800

    Add project files.

---
+++ IST4310FInalProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,7 @@
+﻿namespace IST4310FInalProject.Data
+{
+    //dbcontext for entity framework
+    public class ApplicationDbContext
+    {
+    }
+}
---
+++ IST4310FInalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IST4310FInalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
